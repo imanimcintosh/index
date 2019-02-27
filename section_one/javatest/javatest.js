afb596d108c8deab37858eea3161bfb4c7748b78
imani <mcini010@newschool.edu>
2019-02-27 00:12:48 -0500

    javatest

---
+++ section_one/javatest/javatest.js
@@ -0,0 +1,20 @@
+
+
+
+function changeImage()
+{
+element=document.getElementById('example')
+if (element.src.match("out"))
+  {
+  element.src="weaving1-in.jpg";
+  }
+else
+  {
+  element.src="weaving1-out.jpg";
+  }
+}
+
+
+
+
+
