7f47ef86b504ba7daefb496113def689c3aa331f
imani <mcini010@newschool.edu>
2019-03-14 16:18:18 -0400

    responsive ?

---
+++ section_one/collection/messup.js
@@ -0,0 +1,73 @@
+
+var changeColor=document.getElementById("changeColor");
+var energy2=document.getElementById("energy2");
+var energy3=document.getElementById("energy3");
+var energy4=document.getElementById("energy4");
+var energy5=document.getElementById("energy5");
+var energy6=document.getElementById("energy6");
+var energy7=document.getElementById("energy7");
+var energy8=document.getElementById("energy8");
+var energy9=document.getElementById("energy9");
+var i = 0;
+
+function displayImage() {
+i++;
+  if (i == 1){
+    energy.style.display = "block";
+  }else if(i == 2){
+    energy2.style.display = "block";
+  }else if(i == 3){
+    energy3.style.display = "block";
+  }else if(i == 4){
+    energy4.style.display = "block";
+  }else if(i == 5){
+    energy5.style.display = "block";
+  }else if(i == 6){
+    energy6.style.display = "block";
+  }else if(i == 7){
+    energy7.style.display = "block";
+  }else if(i == 8){
+    energy8.style.display = "block";
+  }else if(i == 9){
+    energy9.style.display = "block";
+  }else if(i == 10){
+    energy10.style.display = "block";
+  }else if(i == 11){
+    energy11.style.display = "block";
+  }else if(i == 12){
+    energy12.style.display ="block";
+  }else if(i == 13){
+    energy13.style.display = "block";
+  }else if(i == 14){
+    energy14.style.display = "block";
+  }else if(i == 15){
+    energy15.style.display = "block";
+  }else if(i == 16){
+    energy16.style.display = "block";
+  }else if(i == 17){
+    energy17.style.display = "block";
+  }else if(i == 18){
+    energy18.style.display = "block";
+  }else if(i == 19){
+    energy19.style.display = "block";
+  }else if(i == 20){
+    energy20.style.display = "block";
+  }else if(i == 21){
+    energy21.style.display = "block";
+  }else if(i == 22){
+    energy22.style.display = "block";
+  }else if(i == 23)
+  energy23.style.display = "block";
+  else if(i == 24)
+  energy24.style.display = "block";
+  
+  
+  else{
+    i = 1;
+  }
+}
+
+
+
+
+changeColor.onclick=displayImage
